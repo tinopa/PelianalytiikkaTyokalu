4de4fdc8ea7ff0f05a36e9f0fc5c9136d821afd2
Tino <Tino@LAPTOP-SB9DS6T5>
2021-10-05 10:50:48 +0300

    Add project files.

---
+++ PelianalytiikkaTyökalu/Käyttölliittymätaso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PelianalytiikkaTyökalu
+{
+    class Käyttölliittymätaso
+    {
+        static void Main(string[] args)
+        {
+            //tämä on se varsinainen ohjelma
+            //täällä kysytään käyttäjältä mitä se haluaa tehdä ja toimitaan sen mukaan
+            //varmaan vaan joku iso switch case häsmäkkä
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PelianalytiikkaTyökalu/Tietokantataso.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PelianalytiikkaTyökalu
+{
+    class Tietokantataso
+    {
+        /*
+         * showGames(string developer)
+         * playTimeTotal(string game)
+         * playTimeAverage(string game)
+         * sessionAverageTime(string game)
+         * playerCount(string Game)
+         * showGameSessions(string player, string game)
+         * showGameEvents(int gameSessionID)
+         * highestSpendingPlayer(string game)
+         * showTransactions(string player)
+         * showHighestTransaction(string Game)
+         * 
+         * ehkä
+         * activePlayers(string game)
+         * retentionRate(string Game, ??)
+         */
+    }
+}
