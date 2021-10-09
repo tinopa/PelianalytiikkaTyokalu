6f5b4ea8dcbed98b5b99c13856a27025eeeebffa
Tino <tino.paajanen@hotmail.fi>
2021-10-09 23:10:59 +0300

    Jotain alkeellista: - käyttöliittymä - yks esimerkki kysely

--- PelianalytiikkaTyökalu/Käyttölliittymätaso.cs
+++ PelianalytiikkaTyökalu/Käyttölliittymätaso.cs
@@ -4,12 +4,53 @@ namespace PelianalytiikkaTyökalu
 {
     class Käyttölliittymätaso
     {
+        //tämä on se varsinainen ohjelma
+        //täällä kysytään käyttäjältä mitä se haluaa tehdä ja toimitaan sen mukaan
+        //varmaan vaan joku iso switch case häsmäkkä
         static void Main(string[] args)
         {
-            //tämä on se varsinainen ohjelma
-            //täällä kysytään käyttäjältä mitä se haluaa tehdä ja toimitaan sen mukaan
-            //varmaan vaan joku iso switch case häsmäkkä
-            Console.WriteLine("Hello World!");
+            string server;
+            string databaseName;
+            string uid;
+            string pwd;
+            bool connectionStatus;
+
+            Console.WriteLine("Address of the database: ");
+            server = Console.ReadLine();
+            Console.WriteLine("Name of the database: ");
+            databaseName = Console.ReadLine();
+            Console.WriteLine("Username: ");
+            uid = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            pwd = Console.ReadLine();
+
+
+            Tietokantataso database= new Tietokantataso(server, databaseName, uid, pwd);
+            connectionStatus = database.OpenConnection();
+
+
+            while (connectionStatus)
+            {
+                Console.WriteLine("What are we going to do?\n" +
+                    "1.  Show all games by a developer\n" +
+                    "2. \n"); 
+                //ja jatkuu niin paljon kun halutaan eri kyselyitä
+
+                int valinta = Convert.ToInt32(Console.ReadLine());
+
+                switch (valinta)
+                {
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                    case 1:
+                        Console.WriteLine("Give the name of the developer: ");
+                        string nimi = Console.ReadLine();
+                        database.ShowAllGamesByDeveloper(nimi);
+                        break;
+                }
+            }
+
         }
     }
 }
--- PelianalytiikkaTyökalu/Tietokantataso.cs
+++ PelianalytiikkaTyökalu/Tietokantataso.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace PelianalytiikkaTyökalu
 {
@@ -22,5 +25,54 @@ namespace PelianalytiikkaTyökalu
          * activePlayers(string game)
          * retentionRate(string Game, ??)
          */
+        public string connetionString = null;
+        public MySqlConnection cnn;
+        
+
+        public Tietokantataso(string server, string database, string uid, string pwd)
+        {
+            connetionString = "server=" + server + ";database="+ database + ";uid=" + uid + ";pwd=" + pwd + ";";
+            cnn = new MySqlConnection(connetionString);
+        }
+
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                cnn.Open();
+                Console.WriteLine("Connection Open ! ");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can not open connection ! ");
+                return false;
+            }
+        }
+
+
+        public MySqlDataReader DatabaseQuery(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, cnn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            return reader;
+        }
+
+
+        public void ShowAllGamesByDeveloper(string nimi)
+        {
+            MySqlDataReader reader = DatabaseQuery("SELECT Peli.Nimi FROM  Peli, Pelistudio WHERE  Peli.Studio_ID = Pelistudio.Studio_ID AND pelistudio.Nimi = " + nimi + ";");
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    string gameName = reader.GetString(reader.GetOrdinal("Nimi"));
+                    Console.WriteLine(gameName);
+                }
+            }
+        }
     }
 }
